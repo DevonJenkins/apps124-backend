4747d31cbbfdcc26531cdd6b511b7491fc452ad3
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-14 13:27:48 -0400

    deploy

--- api/models/app.py
+++ api/models/app.py
@@ -7,14 +7,15 @@ class App(db.Model):
   name = db.Column(db.String(100))
   appType = db.Column(db.String(100))
   description = db.Column(db.String(250))
+  author = db.Column(db.String(100))
   photo = db.Column(db.String(100))
   link = db.Column(db.String(100))
   created_at = db.Column(db.DateTime, default=datetime.utcnow)
   profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
-  profile_name = db.Column(db.Integer, db.ForeignKey('profiles.name'))
+
 
   def __repr__(self):
-    return f"Cat('{self.id}', '{self.name}'"
+    return f"App('{self.id}', '{self.name}'"
 
   def serialize(self):
     app = {c.name: getattr(self, c.name) for c in self.__table__.columns}
